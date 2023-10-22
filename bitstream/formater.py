fd2fe038237ad95d04aa4be2e00d45009f1f5491
Lazar Djurovic <lazardjurovic59@gmail.com>
2023-10-22 20:14:14 +0200

    extracting data for HH GH GG and HG

--- bitstream/formater.py
+++ bitstream/formater.py
@@ -10,7 +10,6 @@ pattern = re.compile(r'0([01]{71})111')
 matches = pattern.findall(lines)
 
 columns = []
-
 # Extract 160 columns
 for i in range(160):
     if i < len(matches):
@@ -23,6 +22,11 @@ merged_binary_array = ''.join(columns)
 #print("Unwrapped bitstream file.")
 #print(merged_binary_array)
 
+def convert_int(list):
+    ret = []
+    for element in list:
+        ret.append(int(element))
+    return ret
 #JUST DO OPERATIONS ON PRE-GENERATED BITSTREAM
 
 filename = "SITE_DEMO.RBT"
@@ -30,11 +34,29 @@ filename = "SITE_DEMO.RBT"
 with open(filename,'r') as f:
     line = f.read();
 
-cell = []
+hh = []
+gh = []
+gg = []
+hg = []
 
 for i in range(643,1921,71):
     for j in range(0,8):
-        print(i+j,line[i+j])
-        cell.append(line[i+j])
+        hh.append(line[i+j])
         
-print(cell)
+for i in range(651,1929,71):
+    for j in range(0,8):
+        gh.append(line[i+j])
+
+for i in range(1929,3207,71):
+    for j in range(0,8):
+        gg.append(line[i+j])
+
+for i in range(1921,3199,71):
+    for j in range(0,8):
+        hg.append(line[i+j])
+
+hh = convert_int(hh)
+print(hh)
+#print_rect(gh)
+#print_rect(gg)
+#print_rect(hg) 
