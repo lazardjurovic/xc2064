00470b775efb90540844a6c3ff71de2149a98aac
Lazar Djurovic <lazardjurovic59@gmail.com>
2024-01-12 21:49:49 +0100

    Connected all CLB pips; ready for testing

--- src/clb_pips.hpp
+++ src/clb_pips.hpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace sc_core;
 using namespace sc_dt;
 
-#define CLB_PIP_SIGNALS_COUNT 26
+#define CLB_PIP_SIGNALS_COUNT 47
 #define CLB_PIPS_COUNT 27
 
 SC_MODULE(clb_pips){
@@ -92,6 +92,76 @@ SC_MODULE(clb_pips){
         pips[12]->ports[1](ports[9]);
         pips[12]->ports[2](*pip_signals[26]); // not connected anywhere else
         pips[12]->ports[3](*pip_signals[24]);
+
+        pips[13]->ports[0](*pip_signals[6]);
+        pips[13]->ports[1](*pip_signals[27]);
+        pips[13]->ports[2](*pip_signals[28]);
+        pips[13]->ports[3](*pip_signals[29]); // not connected anywhere else
+
+        pips[14]->ports[0](*pip_signals[10]);
+        pips[14]->ports[1](*pip_signals[30]);
+        pips[14]->ports[2](ports[18]);
+        pips[14]->ports[3](*pip_signals[27]);
+
+        pips[15]->ports[0](*pip_signals[12]);
+        pips[15]->ports[1](*pip_signals[31]);
+        pips[15]->ports[2](*pip_signals[32]);
+        pips[15]->ports[3](*pip_signals[30]);
+
+        pips[16]->ports[0](*pip_signals[14]);
+        pips[16]->ports[1](*pip_signals[33]);
+        pips[16]->ports[2](ports[16]);
+        pips[16]->ports[3](*pip_signals[31]);
+
+        pips[17]->ports[0](*pip_signals[16]);
+        pips[17]->ports[1](*pip_signals[34]);
+        pips[17]->ports[2](*pip_signals[35]);
+        pips[17]->ports[3](*pip_signals[33]);
+
+        pips[18]->ports[0](*pip_signals[21]);
+        pips[18]->ports[1](*pip_signals[36]);
+        pips[18]->ports[2](ports[14]);
+        pips[18]->ports[3](*pip_signals[34]);
+
+        pips[19]->ports[0](*pip_signals[23]);
+        pips[19]->ports[1](*pip_signals[37]);
+        pips[19]->ports[2](*pip_signals[38]);
+        pips[19]->ports[3](*pip_signals[36]);
+
+        pips[20]->ports[0](*pip_signals[39]); // not connected anywhere else
+        pips[20]->ports[1](ports[10]);
+        pips[20]->ports[2](ports[12]);
+        pips[20]->ports[3](*pip_signals[37]);
+
+        pips[21]->ports[0](*pip_signals[38]);
+        pips[21]->ports[1](*pip_signals[40]);
+        pips[21]->ports[2](*pip_signals[41]);
+        pips[21]->ports[3](*pip_signals[42]); // not connected anywhere else
+
+        pips[22]->ports[0](*pip_signals[25]);
+        pips[22]->ports[1](ports[11]);
+        pips[22]->ports[2](ports[13]);
+        pips[22]->ports[3](*pip_signals[40]);
+
+        pips[23]->ports[0](*pip_signals[28]);
+        pips[23]->ports[1](*pip_signals[43]);
+        pips[23]->ports[2](ports[19]);
+        pips[23]->ports[3](ports[20]);
+        
+        pips[24]->ports[0](*pip_signals[32]);
+        pips[24]->ports[1](*pip_signals[44]);
+        pips[24]->ports[2](ports[17]);
+        pips[24]->ports[3](*pip_signals[43]);
+
+        pips[25]->ports[0](*pip_signals[35]);
+        pips[25]->ports[1](*pip_signals[45]); // ? try finding better solution bcs of pips[7]
+        pips[25]->ports[2](ports[16]);
+        pips[25]->ports[3](*pip_signals[44]);
+
+        pips[26]->ports[0](*pip_signals[41]);
+        pips[26]->ports[1](*pip_signals[46]); // not connected anywhere else
+        pips[26]->ports[2](ports[14]);
+        pips[26]->ports[3](*pip_signals[45]);
 
     }
     private:
